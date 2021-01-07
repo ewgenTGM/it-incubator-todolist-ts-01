afd0ee8b229331ec782e0aa8d4d73ac55323982a
ewgenTGM <ewgenbi@gmail.com>
2021-01-07 12:02:42 +0300

    Initial commit

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -10,7 +10,7 @@ export type TaskType = {
 export type TodoListPropsType = {
   label: string,
   date: string,
-  tasks: Array<TaskType>,
+  tasks: Array<TaskType>
 }
 
 const TodoList = (props: TodoListPropsType) => {
