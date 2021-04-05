86bad65121a2824d5b2e06159956b132ce8445bc
ewgenTGM <ewgenbi@gmail.com>
2021-04-05 20:43:56 +0300

    Подправил API + добавил немного типизации

--- src/stories/Api.stories.tsx
+++ src/stories/Api.stories.tsx
@@ -1,277 +1,266 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { todoApi } from '../utils/api';
+import React, {useEffect, useState} from 'react';
+import {todoApi} from '../utils/api';
 
 export default {
   title: 'API'
 };
 
-const mySuperAxios = axios.create( {
-  baseURL: 'https://social-network.samuraijs.com/api/1.0',
-  withCredentials: true,
-  headers: {
-    'API-KEY': 'a3f1a737-1827-41ef-9c53-49394bbbe1b8'
-  }
-} );
-
 export const AuthMe = () => {
-  const [ state, setState ] = useState<any>( null );
+  const [state, setState] = useState<any>(null);
 
-
-  useEffect( () => {
-    todoApi.authMe().then( res => setState( res ) );
-  }, [] );
+  useEffect(() => {
+    todoApi.authMe().then(res => setState(res));
+  }, []);
   return <div>
-    <pre>{ JSON.stringify( state, null, 2 ) }</pre>
+    <pre>{JSON.stringify(state, null, 2)}</pre>
   </div>;
 };
 
-
 export const GetTodoLists = () => {
-  const [ state, setState ] = useState<any>( null );
-  useEffect( () => {
-    todoApi.getTodoLists().then( setState );
-  }, [] );
+  const [state, setState] = useState<any>(null);
+  useEffect(() => {
+    todoApi.getTodoLists().then(setState);
+  }, []);
 
   return <div>
-    <pre>{ JSON.stringify( state, null, 2 ) }</pre>
+    <pre>{JSON.stringify(state, null, 2)}</pre>
   </div>;
 };
 
 export const CreateTodolist = () => {
-  const [ state, setState ] = useState<any>( null );
-  const [ title, setTitle ] = useState<string>( '' );
+  const [state, setState] = useState<any>(null);
+  const [title, setTitle] = useState<string>('');
   const addTodoList = () => {
-    if ( title.trim().length !== 0 ) {
-      todoApi.createTodoList( title ).then( res => setState( res.data ) );
-      setTitle( '' );
+    if (title.trim().length !== 0) {
+      todoApi.createTodoList(title).then(setState);
+      setTitle('');
     }
   };
 
   return (
       <div>
-        <label htmlFor={ 'title' }>New todo list title:</label><br/>
+        <label htmlFor={'title'}>New todo list title:</label><br/>
         <input
-            style={ { width: '300px', margin: '10px 0' } }
+            style={{width: '300px', margin: '10px 0'}}
             type="text"
-            name={ 'title' }
-            placeholder={ 'Type todo list title' }
-            value={ title }
-            onChange={ ( e ) => setTitle( e.currentTarget.value ) }/><br/>
-        <button onClick={ addTodoList }>Add todo list</button>
+            name={'title'}
+            placeholder={'Type todo list title'}
+            value={title}
+            onChange={(e) => setTitle(e.currentTarget.value)}/><br/>
+        <button onClick={addTodoList}>Add todo list</button>
         <br/>
-        <pre>{ JSON.stringify( state, null, 2 ) }</pre>
+        <pre>{JSON.stringify(state, null, 2)}</pre>
       </div> );
 };
 
 export const DeleteTodolist = () => {
-  const [ state, setState ] = useState<any>( null );
-  const [ todoId, setTodoId ] = useState<string>( '' );
+  const [state, setState] = useState<any>(null);
+  const [todoId, setTodoId] = useState<string>('');
   const deleteTodoList = () => {
-    if ( todoId.trim().length !== 0 ) {
-      todoApi.deleteTodolist( todoId ).then( setState );
-      setTodoId( '' );
+    if (todoId.trim().length !== 0) {
+      todoApi.deleteTodolist(todoId).then(setState);
+      setTodoId('');
     }
   };
 
   return (
       <div>
-        <label htmlFor={ 'title' }>Todo list ID for delete:</label><br/>
+        <label htmlFor={'title'}>Todo list ID for delete:</label><br/>
         <input
-            style={ { width: '300px', margin: '10px 0' } }
+            style={{width: '300px', margin: '10px 0'}}
             type="text"
-            name={ 'title' }
-            placeholder={ 'Type todo list ID' }
-            value={ todoId }
-            onChange={ ( e ) => setTodoId( e.currentTarget.value ) }/><br/>
-        <button onClick={ deleteTodoList }>Delete todo list</button>
+            name={'title'}
+            placeholder={'Type todo list ID'}
+            value={todoId}
+            onChange={(e) => setTodoId(e.currentTarget.value)}/><br/>
+        <button onClick={deleteTodoList}>Delete todo list</button>
         <br/>
-        <pre>{ JSON.stringify( state, null, 2 ) }</pre>
+        <pre>{JSON.stringify(state, null, 2)}</pre>
       </div> );
 };
 
 export const UpdateTodoTitle = () => {
-  const [ title, setTitle ] = useState<any>( null );
-  const [ todoId, setTodoId ] = useState<string>( '' );
-  const [ state, setState ] = useState<any>( null );
+  const [title, setTitle] = useState<any>(null);
+  const [todoId, setTodoId] = useState<string>('');
+  const [state, setState] = useState<any>(null);
   const updateTitle = () => {
-    if ( todoId.trim().length !== 0 ) {
-      todoApi.updateTodoListTitle( todoId, title ).then( setState );
-      setTodoId( '' );
-      setTitle( '' );
+    if (todoId.trim().length !== 0) {
+      todoApi.updateTodoListTitle(todoId, title).then(setState);
+      setTodoId('');
+      setTitle('');
     }
   };
 
   return (
       <div>
-        <label htmlFor={ 'todo_id' }>Todo list Id for update:</label><br/>
+        <label htmlFor={'todo_id'}>Todo list Id for update:</label><br/>
         <input
-            style={ { width: '300px', margin: '10px 0' } }
+            style={{width: '300px', margin: '10px 0'}}
             type="text"
-            name={ 'todo_id' }
-            placeholder={ 'Type todo list Id' }
-            value={ todoId }
-            onChange={ ( e ) => setTodoId( e.currentTarget.value ) }/><br/>
-        <label htmlFor={ 'title' }>New title:</label><br/>
+            name={'todo_id'}
+            placeholder={'Type todo list Id'}
+            value={todoId}
+            onChange={(e) => setTodoId(e.currentTarget.value)}/><br/>
+        <label htmlFor={'title'}>New title:</label><br/>
         <input
-            style={ { width: '300px', margin: '10px 0' } }
+            style={{width: '300px', margin: '10px 0'}}
             type="text"
-            name={ 'title' }
-            placeholder={ 'Type new title' }
-            value={ title }
-            onChange={ ( e ) => setTitle( e.currentTarget.value ) }/><br/>
-        <button onClick={ updateTitle }>Update todo</button>
+            name={'title'}
+            placeholder={'Type new title'}
+            value={title}
+            onChange={(e) => setTitle(e.currentTarget.value)}/><br/>
+        <button onClick={updateTitle}>Update todo</button>
         <br/>
-        <pre>{ JSON.stringify( state, null, 2 ) }</pre>
+        <pre>{JSON.stringify(state, null, 2)}</pre>
       </div> );
 };
 
 export const GetTasks = () => {
-  const [ state, setState ] = useState<any>( null );
-  const [ todoId, setTodoId ] = useState<string>( '' );
+  const [state, setState] = useState<any>(null);
+  const [todoId, setTodoId] = useState<string>('');
 
   const getTasks = () => {
-    if ( todoId.trim().length !== 0 ) {
-      todoApi.getTasks( todoId ).then( setState );
-      setTodoId( '' );
+    if (todoId.trim().length !== 0) {
+      todoApi.getTasks(todoId).then(setState);
+      setTodoId('');
     }
   };
 
   return (
       <div>
-        <label htmlFor={ 'todo_id' }>Todo id:</label><br/>
+        <label htmlFor={'todo_id'}>Todo id:</label><br/>
         <input
-            style={ { width: '300px', margin: '10px 0' } }
+            style={{width: '300px', margin: '10px 0'}}
             type="text"
-            name={ 'todo_id' }
-            placeholder={ 'Type todo list id' }
-            value={ todoId }
-            onChange={ ( e ) => setTodoId( e.currentTarget.value ) }/><br/>
-        <button onClick={ getTasks }>Get tasks</button>
+            name={'todo_id'}
+            placeholder={'Type todo list id'}
+            value={todoId}
+            onChange={(e) => setTodoId(e.currentTarget.value)}/><br/>
+        <button onClick={getTasks}>Get tasks</button>
         <br/>
-        <pre>{ JSON.stringify( state, null, 2 ) }</pre>
+        <pre>{JSON.stringify(state, null, 2)}</pre>
       </div>
   );
 };
 
 export const AddTask = () => {
-  const [ state, setState ] = useState<any>( null );
-  const [ todoId, setTodoId ] = useState<string>( '' );
-  const [ title, setTitle ] = useState<string>( '' );
+  const [state, setState] = useState<any>(null);
+  const [todoId, setTodoId] = useState<string>('');
+  const [title, setTitle] = useState<string>('');
 
   const addTask = () => {
-    if ( todoId.trim().length !== 0 && title.trim().length !== 0 ) {
-      todoApi.addTask( todoId, title ).then( setState );
-      setTitle( '' );
+    if (todoId.trim().length !== 0 && title.trim().length !== 0) {
+      todoApi.addTask(todoId, title).then(setState);
+      setTitle('');
     }
   };
 
   return (
       <div>
-        <label htmlFor={ 'todo_id' }>Todo id:</label><br/>
+        <label htmlFor={'todo_id'}>Todo id:</label><br/>
         <input
-            style={ { width: '300px', margin: '10px 0' } }
+            style={{width: '300px', margin: '10px 0'}}
             type="text"
-            name={ 'todo_id' }
-            placeholder={ 'Type todo list id' }
-            value={ todoId }
-            onChange={ ( e ) => setTodoId( e.currentTarget.value ) }/><br/>
-        <label htmlFor={ 'title' }>Task title:</label><br/>
+            name={'todo_id'}
+            placeholder={'Type todo list id'}
+            value={todoId}
+            onChange={(e) => setTodoId(e.currentTarget.value)}/><br/>
+        <label htmlFor={'title'}>Task title:</label><br/>
         <input
-            style={ { width: '300px', margin: '10px 0' } }
+            style={{width: '300px', margin: '10px 0'}}
             type="text"
-            name={ 'title' }
-            placeholder={ 'Type task title' }
-            value={ title }
-            onChange={ ( e ) => setTitle( e.currentTarget.value ) }/><br/>
-        <button onClick={ addTask }>Add tasks</button>
+            name={'title'}
+            placeholder={'Type task title'}
+            value={title}
+            onChange={(e) => setTitle(e.currentTarget.value)}/><br/>
+        <button onClick={addTask}>Add tasks</button>
         <br/>
-        <pre>{ JSON.stringify( state, null, 2 ) }</pre>
+        <pre>{JSON.stringify(state, null, 2)}</pre>
       </div>
   );
 };
 
 export const DeleteTask = () => {
-  const [ state, setState ] = useState<any>( null );
-  const [ todoId, setTodoId ] = useState<string>( '' );
-  const [ taskId, setTaskId ] = useState<string>( '' );
+  const [state, setState] = useState<any>(null);
+  const [todoId, setTodoId] = useState<string>('');
+  const [taskId, setTaskId] = useState<string>('');
 
   const deleteTask = () => {
-    if ( taskId.trim().length !== 0 && todoId.trim().length !== 0 ) {
-      todoApi.deleteTask( todoId, taskId ).then( setState );
-      setTaskId( '' );
+    if (taskId.trim().length !== 0 && todoId.trim().length !== 0) {
+      todoApi.deleteTask(todoId, taskId).then(setState);
+      setTaskId('');
     }
   };
 
   return (
       <div>
-        <label htmlFor={ 'todo_id' }>Todo id:</label><br/>
+        <label htmlFor={'todo_id'}>Todo id:</label><br/>
         <input
-            style={ { width: '300px', margin: '10px 0' } }
+            style={{width: '300px', margin: '10px 0'}}
             type="text"
-            name={ 'todo_id' }
-            placeholder={ 'Type todo list id' }
-            value={ todoId }
-            onChange={ ( e ) => setTodoId( e.currentTarget.value ) }/><br/>
-        <label htmlFor={ 'task_id' }>Task id for delete:</label><br/>
+            name={'todo_id'}
+            placeholder={'Type todo list id'}
+            value={todoId}
+            onChange={(e) => setTodoId(e.currentTarget.value)}/><br/>
+        <label htmlFor={'task_id'}>Task id for delete:</label><br/>
         <input
-            style={ { width: '300px', margin: '10px 0' } }
+            style={{width: '300px', margin: '10px 0'}}
             type="text"
-            name={ 'task_id' }
-            placeholder={ 'Type task id' }
-            value={ taskId }
-            onChange={ ( e ) => setTaskId( e.currentTarget.value ) }/><br/>
-        <button onClick={ deleteTask }>Delete task</button>
+            name={'task_id'}
+            placeholder={'Type task id'}
+            value={taskId}
+            onChange={(e) => setTaskId(e.currentTarget.value)}/><br/>
+        <button onClick={deleteTask}>Delete task</button>
         <br/>
-        <pre>{ JSON.stringify( state, null, 2 ) }</pre>
+        <pre>{JSON.stringify(state, null, 2)}</pre>
       </div> );
 };
 
-
 export const UpdateTaskTitle = () => {
-  const [ state, setState ] = useState<any>( null );
-  const [ todoId, setTodoId ] = useState<string>( '' );
-  const [ taskId, setTaskId ] = useState<string>( '' );
-  const [ title, setTitle ] = useState<string>( '' );
+  const [state, setState] = useState<any>(null);
+  const [todoId, setTodoId] = useState<string>('');
+  const [taskId, setTaskId] = useState<string>('');
+  const [title, setTitle] = useState<string>('');
 
   const updateTask = () => {
-    if ( taskId.trim().length !== 0 && todoId.trim().length !== 0 && title.trim().length !== 0 ) {
-      todoApi.updateTaskTitle( todoId, taskId, title ).then( setState );
-      setTaskId( '' );
-      setTodoId( '' );
-      setTitle( '' );
+    if (taskId.trim().length !== 0 && todoId.trim().length !== 0 && title.trim().length !== 0) {
+      todoApi.updateTaskTitle(todoId, taskId, title).then(setState);
+      setTaskId('');
+      setTodoId('');
+      setTitle('');
     }
   };
 
   return (
       <div>
-        <label htmlFor={ 'todo_id' }>Todo id:</label><br/>
+        <label htmlFor={'todo_id'}>Todo id:</label><br/>
         <input
-            style={ { width: '300px', margin: '10px 0' } }
+            style={{width: '300px', margin: '10px 0'}}
             type="text"
-            name={ 'todo_id' }
-            placeholder={ 'Type todo list id' }
-            value={ todoId }
-            onChange={ ( e ) => setTodoId( e.currentTarget.value ) }/><br/>
-        <label htmlFor={ 'task_id' }>Task id for delete:</label><br/>
+            name={'todo_id'}
+            placeholder={'Type todo list id'}
+            value={todoId}
+            onChange={(e) => setTodoId(e.currentTarget.value)}/><br/>
+        <label htmlFor={'task_id'}>Task id for delete:</label><br/>
         <input
-            style={ { width: '300px', margin: '10px 0' } }
+            style={{width: '300px', margin: '10px 0'}}
             type="text"
-            name={ 'task_id' }
-            placeholder={ 'Type task id' }
-            value={ taskId }
-            onChange={ ( e ) => setTaskId( e.currentTarget.value ) }/><br/>
-        <label htmlFor={ 'title' }>New title:</label><br/>
+            name={'task_id'}
+            placeholder={'Type task id'}
+            value={taskId}
+            onChange={(e) => setTaskId(e.currentTarget.value)}/><br/>
+        <label htmlFor={'title'}>New title:</label><br/>
         <input
-            style={ { width: '300px', margin: '10px 0' } }
+            style={{width: '300px', margin: '10px 0'}}
             type="text"
-            name={ 'title' }
-            placeholder={ 'Type task title' }
-            value={ title }
-            onChange={ ( e ) => setTitle( e.currentTarget.value ) }/><br/>
-        <button onClick={ updateTask }>Update task</button>
+            name={'title'}
+            placeholder={'Type task title'}
+            value={title}
+            onChange={(e) => setTitle(e.currentTarget.value)}/><br/>
+        <button onClick={updateTask}>Update task</button>
         <br/>
-        <pre>{ JSON.stringify( state, null, 2 ) }</pre>
+        <pre>{JSON.stringify(state, null, 2)}</pre>
       </div>
   );
 };
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,12 +1,61 @@
 import axios from 'axios';
 
-const mySuperAxios = axios.create( {
+const mySuperAxios = axios.create({
   baseURL: 'https://social-network.samuraijs.com/api/1.1',
   withCredentials: true,
   headers: {
     'API-KEY': 'a3f1a737-1827-41ef-9c53-49394bbbe1b8'
   }
-} );
+});
+
+export const todoApi = {
+  authMe() {
+    return mySuperAxios.get<AuthMeResponseType>('auth/me')
+        .then(res => res.data);
+  },
+
+  getTodoLists() {
+    return mySuperAxios.get<GetTodoListsResponseType>('todo-lists')
+        .then(res => res.data);
+  },
+
+  createTodoList(title: string) {
+    return mySuperAxios.post<CreateTodoListResponseType>('todo-lists', {
+      title: title
+    }).then(res => res.data);
+  },
+
+  updateTodoListTitle(todolistId: string, title: string) {
+    return mySuperAxios.put(`todo-lists/${todolistId}`, {
+      title: title
+    }).then(res => res.data);
+  },
+
+  deleteTodolist(todolistId: string) {
+    return mySuperAxios.delete(`todo-lists/${todolistId}`)
+        .then(res => res.data);
+  },
+
+  getTasks(todolistId: string) {
+    return mySuperAxios.get(`todo-lists/${todolistId}/tasks`)
+        .then(res => res.data);
+  },
+
+  addTask(todolistId: string, title: string) {
+    return mySuperAxios.post(`todo-lists/${todolistId}/tasks`, {title})
+        .then(res => res.data);
+  },
+
+  updateTaskTitle(todolistId: string, taskId: string, title: string) {
+    return mySuperAxios.put(`todo-lists/${todolistId}/tasks/${taskId}`, {title})
+        .then(res => res.data);
+  },
+
+  deleteTask(todolistId: string, taskId: string) {
+    return mySuperAxios.delete(`todo-lists/${todolistId}/tasks/${taskId}`)
+        .then(res => res.data);
+  }
+};
 
 type AuthMeResponseType = {
   data: {
@@ -19,30 +68,21 @@ type AuthMeResponseType = {
   resultCode: number
 }
 
-type GetTodoListResponseType = {
+type TodoListDomainType = {
   id: string
   title: string
   addedDate: string
   order: number
 }
 
+type GetTodoListsResponseType = Array<TodoListDomainType>
 
-export const todoApi = {
-  authMe: () => mySuperAxios.get<AuthMeResponseType>( 'auth/me' ).then( res => res.data ),
-  getTodoLists: () => mySuperAxios.get<Array<GetTodoListResponseType>>( 'todo-lists' ).then( res => res.data ),
-  createTodoList: ( title: string ) => mySuperAxios.post( 'todo-lists', {
-    title: title
-  } ).then( res => res.data ),
-  updateTodoListTitle: ( todolistId: string, title: string ) => mySuperAxios.put( `todo-lists/${ todolistId }`, {
-    title: title
-  } ).then( res => res.data ),
-  deleteTodolist: ( todolistId: string ) => mySuperAxios.delete( `todo-lists/${ todolistId }` ).then( res => res.data ),
-  getTasks: ( todolistId: string ) =>
-      mySuperAxios.get( `todo-lists/${ todolistId }/tasks` ).then( res => res.data ),
-  addTask: ( todolistId: string, title: string ) =>
-      mySuperAxios.post( `todo-lists/${ todolistId }/tasks`, { title } ).then( res => res.data ),
-  updateTaskTitle: ( todolistId: string, taskId: string, title: string ) =>
-      mySuperAxios.put( `todo-lists/${ todolistId }/tasks/${ taskId }`, { title } ).then( res => res.data ),
-  deleteTask: ( todolistId: string, taskId: string ) =>
-      mySuperAxios.delete( `todo-lists/${ todolistId }/tasks/${ taskId }` ).then( res => res.data )
-};
+type CreateTodoListResponseType = {
+  resultCode: number
+  messages: Array<string>,
+  data: {
+    item: TodoListDomainType
+  }
+}
+
+
